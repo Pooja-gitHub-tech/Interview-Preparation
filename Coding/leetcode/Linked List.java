a2799f3e06f4ddf5483f8a30ae135d716f5aa682
Pooja-gitHub-tech <76185135+Pooja-gitHub-tech@users.noreply.github.com>
2022-01-20 22:51:44 +0530

    Loop in Linked List

---
+++ Coding/leetcode/Linked List.java
@@ -0,0 +1,32 @@
+/** ----------------------- Question: https://leetcode.com/problems/linked-list-cycle-ii/ ---------------------------------**/
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow;
+        ListNode fast;
+        slow = fast = head;
+        while(fast!=null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
+                slow = head;
+                while(slow != fast) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        
+        return null;
+    }
+}
